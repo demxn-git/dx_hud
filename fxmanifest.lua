30be213fd6d8e89a5c891f07a1240f5b5e8f8bc4
DokaDoka <maxwelljamesmurphy@gmail.com>
2022-10-10 13:05:09 +1100

    style: 4 space indenting

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -7,23 +7,23 @@ lua54 'yes'
 use_experimental_fxv2_oal 'yes'
 
 shared_scripts {
-  '@es_extended/imports.lua',
-  'modules/init.lua',
+    '@es_extended/imports.lua',
+    'modules/init.lua',
 }
 
 client_scripts {
-  'modules/**/client.lua',
-  'client.lua'
+    'modules/**/client.lua',
+    'client.lua'
 }
 
 server_scripts {
-  'modules/**/server.lua'
+    'modules/**/server.lua'
 }
 
 files {
-  'nui/index.html',
-  'nui/**/*',
-  'config.json'
+    'nui/index.html',
+    'nui/**/*',
+    'config.json'
 }
 
 ui_page 'nui/index.html'
