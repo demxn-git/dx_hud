eab7ddc97aceb18b82e68b6a0231bf5dab4651d7
0xDEMXN <15928886+0xDEMXN@users.noreply.github.com>
2023-03-10 02:50:52 +0100

    chore: reordered init functions

--- modules/init.lua
+++ modules/init.lua
@@ -2,19 +2,20 @@ currentResourceName = GetCurrentResourceName()
 config = json.decode(LoadResourceFile(currentResourceName, 'config.json'))
 
 if not IsDuplicityVersion() then
-    playerId = PlayerId()
-    nuiReady = false
-    RegisterNUICallback('nuiReady', function(_, cb)
-        nuiReady = true
-        cb({})
-    end)
-
     function SendMessage(action, message)
         SendNUIMessage({
             action = action,
             message = message
         })
     end
+
+    nuiReady = false
+    RegisterNUICallback('nuiReady', function(_, cb)
+        nuiReady = true
+        cb({})
+    end)
+
+    playerId = PlayerId()
 else
     if config.seatbelt.enabled then SetConvarReplicated('game_enableFlyThroughWindscreen', 'true') end
 end
